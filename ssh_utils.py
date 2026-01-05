5c50ec78b557700c976602149a9b0edf48123b28
Jens Aitola <jjepaah@gmail.com>
2026-01-05 20:21:46 +0200

    Modified establish_ssh_connection function to accept a username parameter that defaults to "none", and only prompts for a password if a username is provided

--- ssh_utils.py
+++ ssh_utils.py
@@ -1,13 +1,13 @@
 import paramiko
 
-def establish_ssh_connection(host, username, password=None, private_key=None):
+def establish_ssh_connection(host, username=None, password=None):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    if private_key:
-        key = paramiko.RSAKey.from_private_key_file(private_key)
-        ssh.connect(hostname=host, username=username, pkey=key)
-    elif password:
+    if username and password:
         ssh.connect(hostname=host, username=username, password=password)
+    elif username:
+        key = paramiko.RSAKey.from_private_key_file("/path/to/private/key")
+        ssh.connect(hostname=host, username=username, pkey=key)
     else:
-        raise ValueError("Either password or private_key must be provided")
+        raise ValueError("Either username or password must be provided")
     return ssh
